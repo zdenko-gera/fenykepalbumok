02168d0765e797b3bd21b3d2a16896104e29f53d
Gera Zdenkó <h157872@stud.u-szeged.hu>
2024-04-24 16:44:57 +0200

    Kategoria oldalon top3 kép (még nem működik jól)

--- category.php
+++ category.php
@@ -11,21 +11,54 @@ require_once('includes/db-conn.php');
 
 $categoryID = $_GET["category_id"];
 
+$stid = oci_parse($conn, 'SELECT * FROM CATEGORIES WHERE CATEGORY_ID = :categoryID');
 
-echo '<h2>Kategória</h2>';
+oci_bind_by_name($stid,':categoryID',$categoryID);
+oci_execute($stid);
+$categoryDetails = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS);
+
+echo '<h2>Legjobb 3 kép a(z) <b>' . $categoryDetails["CATEGORY_NAME"] . '</b> témában:</h2>';
+oci_free_statement($stid);
+
+$top = oci_parse($conn, 'SELECT IMAGES.IMAGE_PATH, IMAGES.IMAGE_ID, ROUND(AVG(PHOTORATING.RATING),2) AS AVG_RATING FROM IMAGES LEFT JOIN photorating ON IMAGES.IMAGE_ID = PHOTORATING.PHOTOID WHERE IMAGES.CATEGORY_ID = :categoryID GROUP BY IMAGES.IMAGE_PATH, IMAGES.IMAGE_ID ORDER BY AVG_RATING DESC');
+
+oci_bind_by_name($top,':categoryID',$categoryID);
+oci_execute($top);
+
+$topThree = oci_fetch_array($top, OCI_ASSOC + OCI_RETURN_NULLS);
+echo '<div class="content-wrapper">';
+while ( $topThree = oci_fetch_array($top, OCI_ASSOC + OCI_RETURN_NULLS)) {
+    echo '<a href="image.php?id='.$topThree["IMAGE_ID"].'" class="main-page-image-wrapper">';
+    echo '<img src="uploadedImages/'.$topThree["IMAGE_PATH"].'" alt="foto" class="my-photo">';
+    echo '<p>Értékelés: '.$topThree["AVG_RATING"].'</p>';
+    echo '</a>';
+}
+echo '</div>';
+oci_free_statement($top);
+
+
+
 
 
 $categoryID = $_GET["category_id"];
-$stmt = oci_parse($conn, 'SELECT IMAGES.IMAGE_PATH, AVG(PHOTORATING.RATING) AS AVG_RATING FROM IMAGES INNER JOIN photorating ON IMAGES.IMAGE_ID = PHOTORATING.PHOTOID WHERE IMAGES.CATEGORY_ID = :categoryID GROUP BY IMAGES.IMAGE_PATH ORDER BY AVG_RATING DESC');
+$stmt = oci_parse($conn, 'SELECT IMAGES.IMAGE_PATH, IMAGES.IMAGE_ID, ROUND(AVG(PHOTORATING.RATING),2) AS AVG_RATING FROM IMAGES LEFT JOIN photorating ON IMAGES.IMAGE_ID = PHOTORATING.PHOTOID WHERE IMAGES.CATEGORY_ID = :categoryID GROUP BY IMAGES.IMAGE_PATH, IMAGES.IMAGE_ID ORDER BY AVG_RATING DESC');
+
 oci_bind_by_name($stmt,':categoryID',$categoryID);
 oci_execute($stmt);
 if (!oci_execute($stmt)) {
     echo 'Nincs a keresésnek megfelelő elem!';
 }
+
+echo '<h2>További képeink a(z) <b>' . $categoryDetails["CATEGORY_NAME"] . '</b> témában:</h2>';
+
+echo '<div class="content-wrapper">';
 while ( $row = oci_fetch_array($stmt, OCI_ASSOC + OCI_RETURN_NULLS)) {
-    echo '<img src="uploadedImages/'.$row["IMAGE_PATH"].'" alt="feltoltott kep" id="image-site-img">';
-    echo '<p>' . $row["AVG_RATING"] . '</p>';
+    echo '<a href="image.php?id='.$row["IMAGE_ID"].'" class="main-page-image-wrapper">';
+    echo '<img src="uploadedImages/'.$row["IMAGE_PATH"].'" alt="foto" class="my-photo">';
+    echo '<p>Értékelés: '.$row["AVG_RATING"].'</p>';
+    echo '</a>';
 }
+echo '</div>';
 
 
 include_once('partials/footer.php');
